46b0863dfdd40cba2d4224ef50abd33cace47345
Sarvagya Patel <patelsarvagya5@gmail.com>
2024-10-26 00:19:42 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api': 'http://localhost:4000'
+      '/api': 'http://68.183.81.222:4000'
     }
   },
   plugins: [react()],
